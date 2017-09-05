31b521cf160d36838977f53e7fe576154c857999
Matt Braden <matt.braden73@gmail.com>
2017-09-05 18:24:50 -0400

    Changed the string a bit to test github on multiple computers

--- visualCS/cSharpProject/cSharpProject/HelloWorldDemo.cs
+++ visualCS/cSharpProject/cSharpProject/HelloWorldDemo.cs
@@ -14,11 +14,11 @@ namespace cSharpProject
             //without anything stopping it, the console will instantly dissapear. To combat this, I'm going to use a simple "press any key to continue" setup
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey(true);
-            Console.WriteLine("What is your name?");
+            Console.WriteLine("\nWhat is your name?");
             //ReadLine() simply reads a line and outputs it, just like Scanner
             String name = Console.ReadLine();
             //If you put a '$' in front of a string, you can access variables by surrounding them with braces
-            Console.WriteLine($"\nHello, {name}");
+            Console.WriteLine($"\nHello, {name}.");
             //code to stop when desired
             Console.WriteLine("Press any key to stop...");
             Console.ReadKey(true);
