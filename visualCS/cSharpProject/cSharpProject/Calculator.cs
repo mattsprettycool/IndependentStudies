76c584adc3e38e0481b0b8b2bcc2af72fc42c21d
realjaisaka <jaitheboss1@gmail.com>
2017-09-06 21:02:17 -0400

    Made a stupid little calculator

---
+++ visualCS/cSharpProject/cSharpProject/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpProject
+{
+    class Calculator
+    {
+        public static void Calc(String [] args)
+        {
+            Boolean run = true;
+            while (run)
+            {
+                Console.WriteLine("Press 1 to calculate\nPress 2 to exit the program.");
+                String input = Console.ReadLine();
+                if(input == "1") { CalcOp(); }
+                else { run = false; }
+            }
+        }
+        public static double CalcOp()
+        {
+            double res = 0;
+            Console.WriteLine("Enter any number: ");
+            double n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick an operator:\n+, -, *, / ");
+            String op = Console.ReadLine();
+            Console.WriteLine("Enter another number: ");
+            double n2 = Convert.ToInt32(Console.ReadLine());
+            if (op.Equals("+")) { res = n1 + n2; }
+            else if (op.Equals("-")) { res = n1 - n2; }
+            else if (op.Equals("*")) { res = n1 * n2; }
+            else { res = n1 / n2; }
+            return res;
+        }
+    }
+}
